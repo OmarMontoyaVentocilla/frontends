85201a75e25f66359949574bf7853e23b8e16a08
OmarMontoyaVentocilla <omarx372@hotmail.com>
2019-08-10 12:17:03 -0500

    Estilizando login

--- pages/login.vue
+++ pages/login.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-app id="login" class="primary">
+  <v-app id="login" class="success">
     <v-content>
       <v-container fluid fill-height>
         <v-layout align-center justify-center>
@@ -7,8 +7,8 @@
             <v-card class="elevation-1 pa-3">
               <v-card-text>
                 <div class="layout column align-center">
-                  <img src="../static/m.png" alt="Vue Material Admin" width="120" height="120">
-                  <h1 class="flex my-4 primary--text">Material Admin Template</h1>
+                  <img src="../static/lk.jpeg" alt="L&K" width="120" height="120">
+                  <h3 class="flex my-4 dark--text">Inversiones y Representaciones</h3>
                 </div>
                 <v-form>
                   <v-text-field
@@ -29,7 +29,7 @@
                 </v-form>
               </v-card-text>
               <v-card-actions>
-                <v-btn icon>
+            <!--    <v-btn icon>
                   <v-icon color="blue">fa fa-facebook-square fa-lg</v-icon>
                 </v-btn>
                 <v-btn icon>
@@ -37,9 +37,9 @@
                 </v-btn>
                 <v-btn icon>
                   <v-icon color="light-blue">fa fa-twitter fa-lg</v-icon>
-                </v-btn>
-                <v-spacer></v-spacer>
-                <v-btn block color="primary" @click="login" :loading="loading">Login</v-btn>
+                </v-btn> <v-spacer></v-spacer> -->
+                
+                <v-btn block color="success" @click="login" :loading="loading">Login</v-btn>
               </v-card-actions>
             </v-card>
           </v-flex>
