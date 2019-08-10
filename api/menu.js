6b5fd702710db86a46a3e193fba088bd1a2b7fe8
OmarMontoyaVentocilla <omarx372@hotmail.com>
2019-08-09 21:34:35 -0500

    Limpiando dashboard v4

--- api/menu.js
+++ api/menu.js
@@ -1,5 +1,6 @@
-const Menu = [
-  { header: 'Pedido' },
+const Menu = [{
+    header: 'Pedido'
+  },
   {
     title: 'Proovedor',
     group: 'Maestras',
@@ -13,32 +14,33 @@ const Menu = [
     icon: 'insert_drive_file',
     href: '/empty'
   },
-  { divider: true },
-  { header: 'Almacen' },
+  {
+    divider: true
+  },
+  {
+    header: 'Almacen'
+  },
   {
     title: 'General',
     group: 'components',
     component: 'components',
     icon: 'tune',
-    items: [
-      { name: 'alerts', title: 'Alerts', href: '/general/alerts' },
-      { name: 'avatars', title: 'Avatars', href: '/general/avatars' },
-      { name: 'badges', title: 'Badges', href: '/general/badges' },
-      { name: 'buttons', title: 'Buttons', href: '/general/buttons' },
-      { name: 'cards', title: 'Cards', href: '/general/cards' },
-      { name: 'carousels', title: 'Carousels', href: '/general/carousels' },
-      { name: 'chips', title: 'Chips', href: '/general/chips' },
-      { name: 'dialogs', title: 'Dialogs', href: '/general/dialogs' },
-      { name: 'icons', title: 'Icons', href: '/general/icons' },
-      { name: 'tables', title: 'Data Tables', href: '/general/tables' },
-      { name: 'parallax', title: 'Parallax  image', href: '/general/parallax' },
-      { name: 'snackbar', title: 'Snackbar', href: '/general/snackbar' },
-      { name: 'progress', title: 'Progress', href: '/general/progress' },
-      { name: 'slider', title: 'Slider', href: '/general/sliders' },
-      { name: 'tooltip', title: 'Tooltip', href: '/general/tooltips' },
-      { name: 'pagination', title: 'Pagination', href: '/general/pagination' },
-      { name: 'typography', title: 'Typography', href: '/general/typography' },
-      { name: 'color', title: 'Color', href: '/general/colors' }
+    items: [{
+        name: 'icons',
+        title: 'Icons',
+        href: '/general/icons'
+      },
+      {
+        name: 'tables',
+        title: 'Data Tables',
+        href: '/general/tables'
+      },
+      {
+        name: 'snackbar',
+        title: 'Snackbar',
+        href: '/general/snackbar'
+      },
+
     ]
   },
   {
@@ -46,8 +48,11 @@ const Menu = [
     group: 'forms',
     component: 'forms',
     icon: 'edit',
-    items: [
-      { name: 'basic', title: 'General', href: '/forms/basic-forms' },
+    items: [{
+        name: 'basic',
+        title: 'General',
+        href: '/forms/basic-forms'
+      },
       {
         name: 'selects',
         title: 'Selects',
@@ -59,12 +64,24 @@ const Menu = [
         title: 'Selection Controls',
         href: '/forms/selection-controls'
       },
-      { name: 'text-fields', title: 'Text Fields', href: '/forms/text-fields' },
-      { name: 'steppers', title: 'Steppers', href: '/forms/steppers' },
-      { name: 'editors', title: 'Editors', href: '/forms/editors' }
+      {
+        name: 'text-fields',
+        title: 'Text Fields',
+        href: '/forms/text-fields'
+      },
+      {
+        name: 'steppers',
+        title: 'Steppers',
+        href: '/forms/steppers'
+      },
+      {
+        name: 'editors',
+        title: 'Editors',
+        href: '/forms/editors'
+      }
     ]
   },
- 
+
 ];
 // reorder menu
 Menu.forEach(item => {
